4578af2d5179e9d89f17941fc39f74d670cff159
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-05-01 14:49:40 +0700

    add controller NvNhapKho

---
+++ Website_CangTienSa/Website_CangTienSa/Controllers/NhanVienNhapKhoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_CangTienSa.Controllers
+{
+    public class NhanVienNhapKhoController : Controller
+    {
+        // GET: NhanVienNhapKho
+        public ActionResult Index_NhanVienNhapKho()
+        {
+            return View();
+        }
+    }
+}
