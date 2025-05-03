d77e233434c494798dad57d816e00c1d764df957
NguyenDinhThi3009 <nguyendinhthi0504@gmail.com>
2025-05-03 22:58:30 +0700

    khach hang, nhan vien xuat kho (lan 1)

---
+++ Website_CangTienSa/Website_CangTienSa/Controllers/KhachHangController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_CangTienSa.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        public ActionResult Index_KhachHang()
+        {
+            ViewBag.Title = "Index_KhachHang";
+            ViewBag.ActiveSidebar = "DangKyGuiHang";
+            return View();
+        }
+
+        public ActionResult XemChiTiet()
+        {
+            ViewBag.Title = "XemChiTiet";
+            ViewBag.ActiveSidebar = "XemChiTietVaLichSu";
+            return View();
+        }
+    }
+}
---
+++ Website_CangTienSa/Website_CangTienSa/Controllers/NhanVienXuatKhoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_CangTienSa.Controllers
+{
+    public class NhanVienXuatKhoController : Controller
+    {
+        // GET: NhanVienXuatKho
+        public ActionResult Index_NhanVienXuatKho()
+        {
+            return View();
+        }
+    }
+}
