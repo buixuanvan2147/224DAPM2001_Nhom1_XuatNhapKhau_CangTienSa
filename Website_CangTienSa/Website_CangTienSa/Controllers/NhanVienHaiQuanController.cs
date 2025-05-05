01e6b1eda38cdec6c44f2c63b4847f3a390efa46
Quancutee <lenguyenxuanquan@gmail.com>
2025-05-05 21:57:02 +0700

    ketoan and haiquan

---
+++ Website_CangTienSa/Website_CangTienSa/Controllers/NhanVienHaiQuanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_CangTienSa.Controllers
+{
+    public class NhanVienHaiQuanController : Controller
+    {
+        // GET: NhanVienHaiQuan
+        public ActionResult IndexHaiQuan()
+        {
+            return View();
+        }
+    }
+}
---
+++ Website_CangTienSa/Website_CangTienSa/Controllers/NhanVienKeToanController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_CangTienSa.Controllers
+{
+    public class NhanVienKeToanController : Controller
+    {
+        // GET: NhanVienKeToan
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
