3f530a892d7d7d2856ca410e4c495a5bd15fd80a
Lê Minh Toàn <tanle16091998@gmail.com>
2025-05-12 02:57:53 +0700

    Cập nhập dữ liệu nhân viên nhập kho

---
+++ Website_CangTienSa/Website_CangTienSa/Models/ContainerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Website_CangTienSa.Models
+{
+    public class ContainerModel
+    {
+        public string MaContainer { get; set; }
+        public string ViTriTrongKho { get; set; }
+        public string KichThuoc { get; set; }
+    }
+}
